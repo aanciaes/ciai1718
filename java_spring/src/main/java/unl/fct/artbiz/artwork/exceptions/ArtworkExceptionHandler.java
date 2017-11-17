3c8363909860a899007ae1d8d10f373b3328f17c
aanciaes <m.anciaes@campus.fct.unl.pt>
2017-11-17 00:41:11 +0000

    Added Artwork exceptions

---
+++ java_spring/src/main/java/unl/fct/artbiz/artwork/exceptions/ArtworkExceptionHandler.java
@@ -0,0 +1,16 @@
+package unl.fct.artbiz.artwork.exceptions;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//@Controller
+public class ArtworkExceptionHandler {
+
+    /*@ExceptionHandler(ArtWorkNotFound.class)
+    @ResponseStatus()
+    public void handleArtWorkNotFound () {
+
+    }*/
+
+}
