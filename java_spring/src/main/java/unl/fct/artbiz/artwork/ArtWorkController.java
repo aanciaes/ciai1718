a93be10ebfd2a4d77960965b89f505b2323537be
aanciaes <m.anciaes@campus.fct.unl.pt>
2017-11-17 00:41:48 +0000

    Artwork controller first version getAll, getById, create, getOnSale

--- java_spring/src/main/java/unl/fct/artbiz/artwork/ArtWorkController.java
+++ java_spring/src/main/java/unl/fct/artbiz/artwork/ArtWorkController.java
@@ -1,7 +1,51 @@
 package unl.fct.artbiz.artwork;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import unl.fct.artbiz.artwork.exceptions.ArtWorkNotFound;
+import unl.fct.artbiz.artwork.model.ArtWork;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping(value = "/artwork")
 public class ArtWorkController {
+
+    //Just for testing
+    private List<ArtWork> artWorks = new ArrayList();
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<ArtWork> getAllArtWorks() {
+        return artWorks;
+    }
+
+    @RequestMapping(value = "/onsale", method = RequestMethod.GET)
+    public List<ArtWork> getOnSalePeices() {
+        return artWorks.stream().filter(artWork -> artWork.isOnSale()).collect(Collectors.toList());
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ArtWork findById(@PathVariable long id) throws ArtWorkNotFound {
+        Optional<ArtWork> matchingArtWork = artWorks.stream().filter(artWork -> artWork.getId() == id).findFirst();
+
+        ArtWork res;
+
+        if ((res = matchingArtWork.orElse(null)) == null) {
+            throw new ArtWorkNotFound();
+        }else {
+            return res;
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ArtWork createArtwork(@RequestBody ArtWork artWork) {
+        ArtWork.validate(artWork);
+        artWorks.add(artWork);
+        return artWork;
+    }
+
+
 }
--- java_spring/src/main/java/unl/fct/artbiz/artwork/model/ArtWork.java
+++ java_spring/src/main/java/unl/fct/artbiz/artwork/model/ArtWork.java
@@ -1,6 +1,110 @@
 package unl.fct.artbiz.artwork.model;
 
+import java.util.Date;
+import java.util.List;
+
 public class ArtWork {
 
+    private long id;
+    private String name;
+    private Date dateOfCreation;
+    private List<String> techniques;
+    private String description;
+    private List<String> keywords;
+    private List<String> multimedia;
+    private long author;
+    private boolean onSale;
+
+    public ArtWork () { }
+
+    public ArtWork(String name, Date dateOfCreation,
+                   List<String> techniques, String description,
+                   List<String> keywords, List<String> multimedia,
+                   long author, boolean onSale) {
+        this.name = name;
+        this.dateOfCreation = dateOfCreation;
+        this.techniques = techniques;
+        this.description = description;
+        this.keywords = keywords;
+        this.multimedia = multimedia;
+        this.author = author;
+        this.onSale = onSale;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateOfCreation() {
+        return dateOfCreation;
+    }
+
+    public void setDateOfCreation(Date dateOfCreation) {
+        this.dateOfCreation = dateOfCreation;
+    }
+
+    public List<String> getTechniques() {
+        return techniques;
+    }
+
+    public void setTechniques(List<String> techniques) {
+        this.techniques = techniques;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    public List<String> getMultimedia() {
+        return multimedia;
+    }
+
+    public void setMultimedia(List<String> multimedia) {
+        this.multimedia = multimedia;
+    }
+
+    public long getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(long author) {
+        this.author = author;
+    }
+
+    public boolean isOnSale() {
+        return onSale;
+    }
+
+    public void setOnSale(boolean onSale) {
+        this.onSale = onSale;
+    }
+
+    public static void validate (ArtWork artWork) {
+        //TODO: Validate schema
+    }
 
 }
