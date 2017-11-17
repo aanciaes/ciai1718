3c5cbf5e69be35ea281bb5f57e8eabfc906702bd
aanciaes <m.anciaes@campus.fct.unl.pt>
2017-11-17 17:02:51 +0000

    SSL with self signed cert

--- java_spring/src/test/java/artworks/ArtworkJUnit.java
+++ java_spring/src/test/java/artworks/ArtworkJUnit.java
@@ -3,19 +3,12 @@ package artworks;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import unl.fct.artbiz.Application;
 import unl.fct.artbiz.artwork.model.ArtWork;
 
@@ -33,7 +26,6 @@ public class ArtworkJUnit {
     @Test
     public void getAllPieces () throws Exception {
         ResponseEntity res = restTemplate.exchange("/artwork", HttpMethod.GET, HttpEntity.EMPTY, List.class);
-
         assert res.getStatusCodeValue() == 200;
         assert ((List) res.getBody()).isEmpty();
     }
@@ -48,6 +40,29 @@ public class ArtworkJUnit {
         HttpEntity<ArtWork> entity = new HttpEntity(artwork);
         ResponseEntity res = restTemplate.exchange("/artwork", HttpMethod.POST, entity, ArtWork.class);
 
+        assert res.getStatusCodeValue() == 200;
+
+        ArtWork response = (ArtWork) res.getBody();
+
+        assert response.getId() == artwork.getId();
+        assert response.getName().equals(artwork.getName());
+
+        ResponseEntity getRes = restTemplate.exchange("/artwork", HttpMethod.GET, HttpEntity.EMPTY, List.class);
+
+        assert getRes.getStatusCodeValue() == 200;
+        assert ((List) getRes.getBody()).size() == 1;
+    }
+
+    @Test
+    //TODO
+    public void addSameArtwork () {
+        ArtWork artwork = new ArtWork("FakeArtwork", new Date(System.currentTimeMillis()),
+                new ArrayList<String>(), "description",
+                new ArrayList<>(),
+                new ArrayList<>(), 1l, false);
+
+        HttpEntity<ArtWork> entity = new HttpEntity(artwork);
+        ResponseEntity res = restTemplate.exchange("/artwork", HttpMethod.POST, entity, ArtWork.class);
         assert res.getStatusCodeValue() == 200;
 
         ArtWork response = (ArtWork) res.getBody();
