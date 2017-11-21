11f247ed45002e56b88b5f91a3583b9dc2925510
aanciaes <m.anciaes@campus.fct.unl.pt>
2017-11-21 23:12:24 +0000

    Added CRUD repository to artworks

--- java_spring/src/main/java/unl/fct/artbiz/artwork/ArtWorkController.java
+++ java_spring/src/main/java/unl/fct/artbiz/artwork/ArtWorkController.java
@@ -3,12 +3,10 @@ package unl.fct.artbiz.artwork;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import unl.fct.artbiz.artwork.exceptions.ArtWorkNotFound;
-import unl.fct.artbiz.artwork.exceptions.DuplicateIdArtwork;
 import unl.fct.artbiz.artwork.model.ArtWork;
-import unl.fct.artbiz.artwork.model.ArtworkRepository;
+import unl.fct.artbiz.artwork.services.ArtworkService;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -17,58 +15,57 @@ public class ArtWorkController {
 
 
     @Autowired
-    private ArtworkRepository artRepository;
+    private ArtworkService artworkService;
 
     @RequestMapping(method = RequestMethod.GET)
     public List<ArtWork> getAllArtWorks() {
-        return artRepository.getAll();
+        return artworkService.getAllPieces();
     }
 
     @RequestMapping(value = "/onsale", method = RequestMethod.GET)
     public List<ArtWork> getOnSalePieces() {
-        return artRepository.getOnSalePieces();
+        return artworkService.getPiecesOnSalePieces();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ArtWork findById(@PathVariable long id) throws ArtWorkNotFound {
-        return artRepository.findById(id);
+        return artworkService.findById(id);
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public ArtWork createArtwork(@RequestBody ArtWork artWork) {
-        return artRepository.save(artWork);
+        return artworkService.createPiece(artWork);
     }
 
     @RequestMapping(method = RequestMethod.PUT)
     public ArtWork updateArtwork(@RequestBody ArtWork artWork) {
-        return artRepository.updatePiece(artWork);
+        return artworkService.updatePiece(artWork);
     }
 
     @RequestMapping(value = "/search/artist/{id}", method = RequestMethod.GET)
     public List<ArtWork> searchByArtist (@PathVariable long id) {
-        return  artRepository.getByArtist(id);
+        return  artworkService.getPiecesByArtist(id);
     }
 
     @RequestMapping(value = "/search/keywords", method = RequestMethod.GET)
     public List<ArtWork> searchByKeyword (@RequestParam String keywords) {
-
         List<String> keywordsAsList = Arrays.asList(keywords.split("\\s"));
 
-        return artRepository.getByKeywords(keywordsAsList);
+        return artworkService.getPiecesByKeywords(keywordsAsList);
     }
 
     @RequestMapping(value = "/{pieceId}/sell", method = RequestMethod.PUT)
     public double sellPiece (@PathVariable long pieceId, @RequestParam("price") double price) {
-        ArtWork artWork = artRepository.findById(pieceId);
+        ArtWork artWork = artworkService.findById(pieceId);
         artWork.setOnSale(true);
         artWork.setPrice(price);
-        return artRepository.updatePiece(artWork).getPrice();
+        return artworkService.updatePiece(artWork).getPrice();
     }
 
     @RequestMapping(value = "/{pieceId}/keep", method = RequestMethod.PUT)
     public boolean keepPiece (@PathVariable long pieceId) {
-        ArtWork artWork = artRepository.findById(pieceId);
+        ArtWork artWork = artworkService.findById(pieceId);
         artWork.setOnSale(false);
-        return artRepository.updatePiece(artWork).isOnSale();
+        return artworkService.updatePiece(artWork).isOnSale();
     }
 }
