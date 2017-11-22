ab13450a1b91ca8b0b9eb61e8b0377d69b22dff1
aanciaes <m.anciaes@campus.fct.unl.pt>
2017-11-22 12:31:41 +0000

    Changed storage exceptions response status

--- java_spring/src/main/java/unl/fct/artbiz/storage/exceptions/StorageFileNotFoundException.java
+++ java_spring/src/main/java/unl/fct/artbiz/storage/exceptions/StorageFileNotFoundException.java
@@ -1,5 +1,9 @@
 package unl.fct.artbiz.storage.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class StorageFileNotFoundException extends StorageException {
 
     public StorageFileNotFoundException(String message) {
