179595894014ef3b4a488cc4008f69895bf215fe
aanciaes <m.anciaes@campus.fct.unl.pt>
2017-11-28 14:52:13 +0000

    small update. uncomment cors line on security config

--- java_spring/src/main/java/unl/fct/artbiz/auth/config/SecurityConfig.java
+++ java_spring/src/main/java/unl/fct/artbiz/auth/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.cors().and()
+                .cors().and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, publicGetEndpoints).permitAll()
