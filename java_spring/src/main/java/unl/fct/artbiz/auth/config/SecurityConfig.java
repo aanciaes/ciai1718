0222fb5b5015e7a487f0f44a9721a04b2137299d
aanciaes <m.anciaes@campus.fct.unl.pt>
2017-11-28 11:59:35 +0000

    Added cors to http config

--- java_spring/src/main/java/unl/fct/artbiz/auth/config/SecurityConfig.java
+++ java_spring/src/main/java/unl/fct/artbiz/auth/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, publicGetEndpoints).permitAll()
                 .and()
